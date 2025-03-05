57794f419df95a336f573bdb4a5c0bd25cdc0144
nagababu8833 <167737066+nagababu8833@users.noreply.github.com>
2025-03-05 07:42:55 +0530

    Create terraform-azure-rg-vnet.tf

---
+++ terraform-azure-rg-vnet.tf
@@ -0,0 +1,22 @@
+Here's a smaller Terraform file to create an Azure Resource Group and a Virtual Network:
+
+main.tf
+
+```
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+```
